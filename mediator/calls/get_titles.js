ca3936b937187688925022bd6a10a18b471447c9
vincent villaluna <villalunavincent@gmail.com>
2021-06-08 16:41:59 +0800

    :rocket:

--- mediator/calls/get_titles.js
+++ mediator/calls/get_titles.js
@@ -1,5 +1,5 @@
 const getTitles = async () => {
-  const titles = await fetch('https://i7s1kv7l.api.sanity.io/v1/data/query/production?query=*%5B_type%3D%3D%22post%22%5D%7B%0A%20%20title%2C%0A%20%20_id%2C%0A%20%20slug%0A%7D');
+  const titles = await fetch('sanity.titles');
   return titles.json();
 }
 
